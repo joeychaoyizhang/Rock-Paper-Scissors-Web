f3c538de753ec810391bf1c13f331c1d0606f891
joeychaoyizhang <zhangchaoyi551@gmail.com>
2024-06-26 16:18:34 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+async function getgame(userMove) {
+  var result = document.getElementById("result");
+  try{
+    var response = await fetch("https://cf6fc9b5-7461-4419-a6f8-16bca4aa156b-00-3rucax5zlh9m3.spock.replit.dev/Game", {
+      method: "POST",
+      headers:{
+        "Content-Type":"application/json"
+      },
+      body: JSON.stringify({move:userMove})
+    });
+    var data = await response.json();
+
+    var gameResult = "<p><strong>Your Move: <strong>" +data.user+ "</p>";
+    gameResult+= "<p><strong>Computer Move: <strong>" +data.comp+ "</p>";
+    gameResult+= "<p><strong>Result: <strong>" +data.result+ "</p>";
+    gameResult+= "<p><strong>Total Wins: <strong>" +data.win+ "</p>";
+    gameResult+= "<p><strong>Total Ties: <strong>" +data.tied+ "</p>";
+    gameResult+= "<p><strong>Total Lose: <strong>" +data.lose+ "</p>";
+    result.innerHTML = gameResult;
+  } catch(err){
+    console.log("err",err);
+  }
+}
